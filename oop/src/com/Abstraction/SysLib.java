ef602f02a889d0e7e6c1c45101c0a09c4d3009ba
DanKoyuki <95008109+DanKoyuki@users.noreply.github.com>
2023-05-03 12:23:52 +0700

    Add files via upload

---
+++ oop/src/com/Abstraction/SysLib.java
@@ -0,0 +1,6 @@
+package com.Abstraction;
+
+public abstract class SysLib {
+	abstract void bbook(String title, BooksReader book);
+	abstract String findBook(String title, BooksReader book);
+}
---
+++ oop/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Irwanto Danang
+ *
+ */
+module oop {
+}
